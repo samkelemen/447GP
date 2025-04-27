b5445f008d94d7c2fd8011a98830051a1b9167fd
TheOfficialBarn <apbarnard2004@icloud.com>
2025-04-27 17:29:23 -0500

    comment

--- Part5/printtables.sql
+++ Part5/printtables.sql
@@ -1,5 +1,5 @@
 -- Membership
-SELECT 'Membership'   AS TableName;
+SELECT 'Membership'   AS TableName; -- This is the closest thing I could get to a print statement for each table name
 SELECT * FROM Membership;
 
 -- Client
