3acfab00e5118993e2bc371f9d18bf1c11487a77
TheOfficialBarn <apbarnard2004@icloud.com>
2025-04-27 17:13:47 -0500

    Added a lil printtables.sql file

---
+++ Part5/printtables.sql
@@ -0,0 +1,19 @@
+SELECT * FROM Membership;
+SELECT * FROM Client;
+SELECT * FROM Item;
+SELECT * FROM Book;
+SELECT * FROM DigitalMedia;
+SELECT * FROM Magazine;
+SELECT * FROM Author;
+SELECT * FROM Publisher;
+SELECT * FROM Creates;
+SELECT * FROM Publishes;
+SELECT * FROM Notification;
+SELECT * FROM `Transaction`;
+SELECT * FROM Reservation;
+
+-- Usage:
+-- Uncomment/Comment tables in order to view/hide them when you want to print tables
+-- Use the Command SOURCE to run this
+-- E.g. SOURCE /home/a542b289/Downloads/447GP/Part5/printtables.sql;
+-- Ensure that this file is in your account of the cycle-server in order to use it
